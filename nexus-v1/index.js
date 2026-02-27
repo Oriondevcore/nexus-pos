4c7b775bdff0b672bd0ff9895568da67e78be3c5
Oriondevcore <graham@oriondevcore.com>
2026-02-27 17:48:24 +0200

    wefwg

--- nexus-v1/index.js
+++ nexus-v1/index.js
@@ -7,14 +7,9 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const { setGlobalOptions } = require("firebase-functions");
-const { onRequest } = require("firebase-functions/https");
+const {setGlobalOptions} = require("firebase-functions");
+const {onRequest} = require("firebase-functions/https");
 const logger = require("firebase-functions/logger");
-
-const yoco = require("./yocoPayment");
-exports.createYocoPayment = yoco.createYocoPayment;
-exports.getYocoPaymentStatus = yoco.getYocoPaymentStatus;
-exports.yocoWebhook = yoco.yocoWebhook;
 
 // For cost control, you can set the maximum number of containers that can be
 // running at the same time. This helps mitigate the impact of unexpected
